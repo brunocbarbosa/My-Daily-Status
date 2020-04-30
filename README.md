ab2e408742d58a499a5b8eed41e2d046aba890a0
Bruno Barbosa <bruno.cbarbosa90@gmail.com>
2020-04-30 17:43:45 -0300

    tempo: 1:17:20

--- README.md
+++ README.md
@@ -4,6 +4,9 @@
 - NextJS
 - TailwindCss
 - auth0
+- firebase
+- GeoFirestore
+- axios
 
 ## NextJS
 NextJS is a framework created by Zeit to build a page server-side rendering, apis, statics apps. To install write `npm install next react react-dom`, after in `package.json` write the following script: 
@@ -20,3 +23,9 @@ A layout library `npm install postcss-import postcss-preset-env tailwindcss`. To
 
 ## Auth0
 Auth0 is a tool to control auth, create an account in `https://auth0.com/`, and follow the steps.
+
+## Firebase
+Firebase is a NoSQL database from google, to install the dependence write in terminal `npm install firebase-admin`.
+
+## Geofirestore
+A Firebase lib to work with geolocation to instal write `npm install geofirestore`
--- pages/app.js
+++ pages/app.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import auth0 from '../lib/auth0';
+import router from 'next/router';
+import { db } from '../lib/db';
 
 export default function App(props){
+    useEffect(() => {
+        if(!props.isAuth){
+            router.push('/');
+            alert('Não está logado');
+        }else if(props.forceCreate){
+            router.push('/create-status')
+        }
+    })
+    if(!props.isAuth || props.forceCreate){
+        return null;
+    }
     return(
         <div>
         <h1>APP</h1>
+        <table>
+            { props.checkins.map(checkin => {
+                return(
+                    <tr>
+                        { checkin.id }
+                    </tr>
+                )
+            }) }
+        </table>
         <pre>{JSON.stringify(props, null, 2)}</pre>
         </div>
     );
@@ -13,14 +35,63 @@ export default function App(props){
 export async function getServerSideProps({req, res}){
     const session = await auth0.getSession(req) 
     if(session){
+        const today = new Date()
+        const currentDate = `${today.getFullYear()}'-'${today.getMonth()+1}'-'${today.getDate()}`
+        const todaysCheckin = await db
+                .collection('markers')
+                .doc(currentDate)
+                .collection('checks')
+                .doc(session.user.sub)
+                .get()
+        
+        const todaysData = todaysCheckin.data()
+        let forceCreate = true;
+        if(todaysData){
+            forceCreate = false;
+            const checkins = await db
+                .collection('markers')
+                .doc(currentDate)
+                .collection('checks')
+                .near({
+                    center: todaysData.coordinates,
+                    radius: 1000
+                })
+                .get()
+            
+            const checkinsList = []
+            
+            checkins.docs.forEach(doc => {
+                checkinsList.push({
+                    id: doc.id,
+                    status: doc.data().status,
+                    coords: {
+                        lat: doc.data().coordinates.latitude,
+                        long: doc.data().coordinates.longitude
+                    }
+                })
+            })
+
+            return{
+                props: {
+                    isAuth: true,
+                    user: session.user,
+                    forceCreate: false,
+                    checkins: checkinsList
+                }
+            }
+        }
+
         return{
             props: {
-                user: session.user
+                isAuth: true,
+                user: session.user,
+                forceCreate
             }
         }
     }
     return {
         props:{
+            isAuth: false,
             user: {}
         }
     }
